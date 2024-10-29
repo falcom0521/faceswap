11089d2926a02df3bb0e31cd59ae6f5a5e742c27
Ajay Paul <ajaypaul@Ajays-MacBook-Air.local>
2024-10-29 14:25:55 +0530

    api request fixed

--- face-swap-project/backend/server.js
+++ face-swap-project/backend/server.js
@@ -9,14 +9,8 @@ const port = 3000;
 
 app.use(bodyParser.json({ limit: "10mb" }));
 
-const api_key = "SG_c3d9e489d5d80d77";
-const url = "https://api.segmind.com/v1/faceswap-v2";
-
-// Function to convert image to base64
-async function toB64(imgPath) {
-  const data = fs.readFileSync(path.resolve(imgPath));
-  return Buffer.from(data).toString("base64");
-}
+const api_key = "SG_62b0bbd802f009fb";
+const url = "https://api.segmind.com/v1/sd2.1-faceswapper";
 
 // Serve frontend files
 app.use(express.static(path.join(__dirname, "../frontend")));
@@ -27,31 +21,21 @@ app.use("/assets", express.static(path.join(__dirname, "../assets")));
 app.post("/faceswap", async (req, res) => {
   try {
     console.log("Face swap request received"); // Log a simple message indicating a request was received
+
     const { sourceImg, targetImg } = req.body;
-    // Decode base64 data for source and target images
-    const sourceImgBuffer = Buffer.from(sourceImg, "base64");
-    const targetImgBuffer = Buffer.from(targetImg, "base64");
-
-    // Write target image to a temporary file
-    // const targetImgPath = path.join(__dirname, "temp", "target_img.jpg"); // Define your temporary directory here
-    // fs.writeFileSync(targetImgPath, targetImgBuffer);
-
-    // Perform face swap
-    // const targetImgB64 = await toB64(targetImgPath);
-
-    // Remove temporary file
-    // fs.unlinkSync(targetImgPath);
 
     const data = {
-      source_img: sourceImg,
-      target_img: targetImg,
-      face_restore: "codeformer-v0.1.0.pth",
+      input_face_image: sourceImg,
+      target_face_image: targetImg,
+      face_restore: "codeformer-v0.1.0.pth", // Adjust based on API requirements
+      file_type: "jpg",
       base64: true,
     };
 
     const response = await axios.post(url, data, {
       headers: { "x-api-key": api_key },
     });
+
     console.log("Face swap successful"); // Log a concise message
     res.json(response.data);
   } catch (error) {
