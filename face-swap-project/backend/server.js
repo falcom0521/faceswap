e7eefd62f0802f4d23c9ea89f3fb115ed1ad4402
Ajay Paul <ajaypaul@Ajays-MacBook-Air.local>
2024-11-22 22:26:40 +0530

    vdfbvd

--- face-swap-project/backend/server.js
+++ face-swap-project/backend/server.js
@@ -9,7 +9,7 @@ const port = 3000;
 
 app.use(bodyParser.json({ limit: "10mb" }));
 
-const api_key = "SG_62b0bbd802f009fb";
+const api_key = "SG_1740a5f52efee94a";
 const url = "https://api.segmind.com/v1/sd2.1-faceswapper";
 
 // Serve frontend files
